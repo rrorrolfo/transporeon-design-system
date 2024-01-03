d4b520ed031eea827afb8b7183c69e44e29b02ec
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-03 15:31:17 +0200

    Created icons components

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+export { ChevronDownIcon } from "./ChevronDownIcon";
+export { CloseIcon } from "./CloseIcon";
+export { DarkModeIcon } from "./DarkModeIcon";
+export { DoneIcon } from "./DoneIcon";
+export { LightModeIcon } from "./LightModeIcon";
+export { Logo } from "./Logo";
