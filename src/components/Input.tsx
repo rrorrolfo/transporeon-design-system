fd78b6ee672bd16d25282679bce34d9eb671aeb2
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-07 12:57:37 +0200

    Created Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,14 @@
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
+  id: string;
+}
+
+const Input = ({ id }: InputProps) => {
+  return (
+    <div>
+      <label htmlFor={id}>label</label>
+      <input type="text" id={id} data-testid={id} />
+    </div>
+  );
+};
+
+export default Input;
