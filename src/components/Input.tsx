26f2eed5d09018c1f35a86a9cf2da34691c0d0f3
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-07 13:51:57 +0200

    Added hideLabel prop to Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,13 @@
 export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
   id: string;
+  labelText?: string;
+  hideLabel?: boolean;
 }
 
-const Input = ({ id }: InputProps) => {
+const Input = ({ id, hideLabel = false, labelText = "" }: InputProps) => {
   return (
     <div>
-      <label htmlFor={id}>label</label>
+      {!hideLabel && <label htmlFor={id}>{labelText}</label>}
       <input type="text" id={id} data-testid={id} name={id} />
     </div>
   );
