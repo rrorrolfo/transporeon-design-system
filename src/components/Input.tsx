a29bea6613b320c80b5c9bac2f16e03a8b957532
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-07 14:05:02 +0200

    Added functionality to display help message for the Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,13 +2,22 @@ export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
   id: string;
   labelText?: string;
   hideLabel?: boolean;
+  showHelpMessage?: boolean;
+  helpMessage?: string;
 }
 
-const Input = ({ id, hideLabel = false, labelText = "" }: InputProps) => {
+const Input = ({
+  id,
+  hideLabel = false,
+  labelText = "",
+  showHelpMessage = false,
+  helpMessage = "",
+}: InputProps) => {
   return (
     <div>
       {!hideLabel && <label htmlFor={id}>{labelText}</label>}
       <input type="text" id={id} data-testid={id} name={id} />
+      {showHelpMessage && <span data-testid="help-message">{helpMessage}</span>}
     </div>
   );
 };
