b357081c7784593a25253f88534e93f50268f575
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-08 09:53:52 +0200

    Styled checkbox component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import "./checkbox.css";
+
 interface CheckboxProps extends React.HTMLAttributes<HTMLInputElement> {
   disabled?: boolean;
   checked?: boolean;
@@ -29,8 +31,12 @@ const Checkbox = ({
   };
 
   return (
-    <div>
-      <label htmlFor={id} data-testid="checkbox-label">
+    <div className="checkbox__wrapper">
+      <label
+        className={`checkbox__label ${disabled ? "disabled" : ""}`}
+        htmlFor={id}
+        data-testid="checkbox-label"
+      >
         <input
           type="checkbox"
           name="checkbox"
@@ -41,12 +47,25 @@ const Checkbox = ({
           disabled={disabled}
           checked={isChecked}
           onChange={handleChange}
+          className={isChecked ? "checked" : ""}
           {...props}
         />
-        {labelText && <span data-testid="label-text">{labelText}</span>}
-        {helperMessage && (
-          <span data-testid="helper-message">{helperMessage}</span>
-        )}
+        <span className="checkbox__checkmark"></span>
+        <div className="checkbox__content-container">
+          {labelText && (
+            <span className="checkbox__label-text" data-testid="label-text">
+              {labelText}
+            </span>
+          )}
+          {helperMessage && (
+            <span
+              className="checkbox__helper-message"
+              data-testid="helper-message"
+            >
+              {helperMessage}
+            </span>
+          )}
+        </div>
       </label>
     </div>
   );
