c04a83e183563ec797f4405a8dc6cbc13aa5f931
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-09 13:56:38 +0200

    Wrapped App component into ThemeContextProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import "./App.css";
+import ThemeContextProvider from "./context";
 
+import "./App.css";
 import Header from "./components/Header";
 import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
-    <div className="main-app__container">
-      <div className="app_container">
-        <Header />
-        <div className="login-form__container">
-          <LoginForm />
+    <ThemeContextProvider>
+      <div className="main-app__container">
+        <div className="app_container">
+          <Header />
+          <div className="login-form__container">
+            <LoginForm />
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeContextProvider>
   );
 }
 
