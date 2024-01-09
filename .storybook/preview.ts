facbcfef75de9fcd16d34defd8bdf07b8b07f92d
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-09 09:58:25 +0200

    Added import for checkbox and input css files

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,8 @@ import type { Preview } from "@storybook/react";
 import "../src/index.css";
 import "../src/components/button.css";
 import "../src/components/checkboxGoup.css";
+import "../src/components/checkbox.css";
+import "../src/components/input.css";
 
 const preview: Preview = {
   parameters: {
