a72cc247a7437190d18cadcf6e6dde449f19b53a
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-09 15:36:03 +0200

    Created hook to detect the current system theme

--- src/context/hooks.ts
+++ src/context/hooks.ts
@@ -1,5 +1,6 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { ThemeContext } from ".";
+import { ThemeOptions } from "../types/types";
 
 export const useThemeContext = () => {
   const context = useContext(ThemeContext);
@@ -9,4 +10,26 @@ export const useThemeContext = () => {
   }
 
   return context;
+};
+
+export const useThemeDetector = (): ThemeOptions => {
+  const getCurrentTheme = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const isDarkTheme = getCurrentTheme();
+
+  const [currentTheme, setCurrentTheme] = useState<ThemeOptions>(
+    isDarkTheme ? "dark" : "light"
+  );
+
+  const themeListener = (event: MediaQueryListEvent) => {
+    setCurrentTheme(event.matches ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    const checkForDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+    checkForDarkTheme.addEventListener("change", themeListener);
+    return () => checkForDarkTheme.removeEventListener("change", themeListener);
+  }, []);
+
+  return currentTheme;
 };
