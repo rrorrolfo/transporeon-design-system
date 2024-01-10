393664a99b8cdbf1b0249c3f2ea2b8876b6feb51
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-10 13:46:06 +0200

    Added addon-docs to project

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,15 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/components/**/*.stories.@(ts|tsx)"],
+  stories: [
+    "../src/components/**/*.stories.@(ts|tsx)",
+    "../src/components/**/*.mdx",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/react-vite",
