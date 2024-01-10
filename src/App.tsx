5de1b8ae4d041a0e88ad961cdf0f46808a50dc32
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-10 11:19:22 +0200

    Moved div container for login form inside login form component

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,14 @@ import ThemeContextProvider from "./context";
 import "./App.css";
 import Header from "./components/Header";
 import LoginForm from "./components/LoginForm";
-import { useThemeContext } from "./context/hooks";
 
 function App() {
-  const { theme } = useThemeContext();
   return (
     <ThemeContextProvider>
       <div className="main-app__container">
         <div className="app_container">
           <Header />
-          <div className={`login-form__container ${theme}`}>
-            <LoginForm />
-          </div>
+          <LoginForm />
         </div>
       </div>
     </ThemeContextProvider>
