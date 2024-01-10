6e267de9092a002f5a7c798fa074d062e65923d7
Rodolfo Perez <rodolfobeste@gmail.com>
2024-01-10 09:12:00 +0200

    Added conditional styling according to selected theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import ThemeContextProvider from "./context";
 import "./App.css";
 import Header from "./components/Header";
 import LoginForm from "./components/LoginForm";
+import { useThemeContext } from "./context/hooks";
 
 function App() {
+  const { theme } = useThemeContext();
   return (
     <ThemeContextProvider>
       <div className="main-app__container">
         <div className="app_container">
           <Header />
-          <div className="login-form__container">
+          <div className={`login-form__container ${theme}`}>
             <LoginForm />
           </div>
         </div>
